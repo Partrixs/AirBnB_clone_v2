be8286c4e6442703fb9a0a237957217196e2e717
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-01-17 08:33:07 -0800

    Revert "all commit reversed"
    This reverts commit ede73b0c64bf9da0111cb3be23f895e38b09cb58.

--- models/base_model.py
+++ models/base_model.py
@@ -18,23 +18,20 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        def __init__(self, *args, **kwargs):
-        """Instantiation of base model class
+        """Initialize a new BaseModel.
         Args:
-            args: it won't be used
-            kwargs: arguments for the constructor of the BaseModel
-        Attributes:
-            id: unique id generated
-            created_at: creation date
-            updated_at: updated date
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if "id" not in kwargs:
+                if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
             if "created_at" not in kwargs:
                 self.created_at = datetime.now()
