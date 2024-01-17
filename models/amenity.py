1433420954cbde480a9e14ff0b7cb702ec4749e4
Phoenix G <120309677+Partrixs@users.noreply.github.com>
2024-01-17 09:26:28 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from models.place import place_amenity
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
 class Amenity(BaseModel, Base):
