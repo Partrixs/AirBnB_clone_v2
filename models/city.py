ac5afae47cd5a10a886755927a6d9af978df0d89
Phoenix G <120309677+Partrixs@users.noreply.github.com>
2024-01-17 08:58:21 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -8,6 +8,7 @@ from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
+
 class City(BaseModel, Base):
     """Represents a city for a MySQL database.
 
@@ -19,3 +20,6 @@ class City(BaseModel, Base):
         state_id (sqlalchemy String): The state id of the City.
     """
     __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
